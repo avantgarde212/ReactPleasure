d1b211f8d1f341e0de3a6add97d25792d9dd0436
avantgarde212 <101116687+avantgarde212@users.noreply.github.com>
2022-08-31 21:11:07 +0900

    Add files via upload

---
+++ 4일차/app.js
@@ -0,0 +1,15 @@
+function sayHello(NameofPerson, age){
+    console.log("Hi I'm " + NameofPerson + " and " + age + " years old" )
+};
+
+sayHello("seoyoon", "33");
+
+sayHello("juyoung", "33");
+
+
+
+
+function plius(Number1, Number2){
+    console.log(Number1 + Number2);
+}
+plius(2,3);
